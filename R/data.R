a3ad841cf7a5c4e0d84511d9ed57b069d52ba84c
Borja Andrino <borja.andrino@gmail.com>
2024-02-11 21:34:48 +0100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Resultados del PP en las elecciones de 2020 en municipios de menos de 20.000 habitantes
+#'
+#' @format Un data frame con 296 filas y 13 columnas.
+#' \describe{
+#' }
+#' @source Instituto Galego de Estadística
+#'
+#' @examples
+#' data(galicia_20_pp_menos_20k)
+"galicia_20_pp_menos_20k"
+
+
+
+#' Resultados del PP en las elecciones de 2020 en municipios de menos de 20.000 habitantes
+#'
+#' @format Un data frame con 350 filas y 9 columnas.
+#' \describe{
+#' }
+#' @source Congreso de los Diputados
+#'
+#' @examples
+#' data(diputados_congreso)
+"diputados_congreso"
+
+
---
+++ data-raw/process_all_datasets.R
@@ -0,0 +1,27 @@
+
+library(tidyverse)
+library(usethis)
+
+galicia_20_pp_menos_20k <-
+  read_csv("data-raw/elecciones_gallegas/historic_elecciones_galicia.csv") |>
+  filter(election == 2020,
+  partido == "PP",
+  censo < 20000)
+
+usethis::use_data(galicia_20_pp_menos_20k, overwrite = T)
+
+
+diputados_congreso <- read_csv2("data-raw/congreso_diputados/DiputadosActivos__20240211050026.csv") |>
+  mutate(GRUPOPARLAMENTARIO = str_replace_all(GRUPOPARLAMENTARIO, "Grupo Parlamentario", "G. P."),
+         GRUPOPARLAMENTARIO = fct_recode(GRUPOPARLAMENTARIO,
+                                         `G. P. Popular` = "G. P. Popular en el Congreso",
+                                         `G. P. Socialista` = "G. P. Socialista",
+                                         `G. P. VOX` = "G. P. VOX",
+                                         `G. P. Vasco` = "G. P. Vasco (EAJ-PNV)",
+                                         `G. P. EH Bildu` = "G. P. Euskal Herria Bildu",
+                                         `G. P. SUMAR` = "G. P. Plurinacional SUMAR",
+                                         `G. P. Republicano` = "G. P. Republicano",
+                                         `G. P. Mixto` = "G. P. Mixto",
+                                         `G. P. Junts` = "G. P. Junts per Catalunya"))
+
+usethis::use_data(diputados_congreso, overwrite = T)
